1d73094801cd7d37f84074bf44c42d908cf56675
Neville Tummon <nt.devs@gmail.com>
2025-11-22 14:52:39 +1100

    extract frontmatter using 3rd party lib

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -6,20 +6,33 @@ from obsidian_api.exceptions import NoteMissingException
 from obsidian_api.note import Note
 from obsidian_api.vault import ObsidianVault
 
+FRONTMATTER1 = """---
+title: About Me
+tags: [personal, introduction]
+---
+"""
+
+FRONTMATTER2 = """---
+title: About Me
+tags:
+  - personal
+  - introduction
+---
+"""
+
+NOTE = f"""
+{FRONTMATTER1}
+
+This is a note about myself. You can find more in [[projects]] and [[hobbies]].
+"""
+
 
 @pytest.fixture
 def sample_note():
-    content = """---
-    title: About Me
-    tags: [personal, introduction]
-    ---
-
-    This is a note about myself. You can find more in [[projects]] and [[hobbies]].
-    """
     return Note(
         slug="about-me",
         filename="test_data/about-me.md",
-        content=content,
+        content=NOTE,
     )
 
 
@@ -31,20 +44,38 @@ def vault():
 def test_note_initialization(sample_note):
     assert sample_note.slug == "about-me"
     assert sample_note.filename == "about-me.md"
-    assert (
-        sample_note.content
-        == """---
-    title: About Me
-    tags: [personal, introduction]
-    ---
-
-    This is a note about myself. You can find more in [[projects]] and [[hobbies]].
-    """
-    )
+    assert sample_note.content == NOTE
     assert sample_note.frontmatter == {
         "title": "About Me",
         "tags": ["personal", "introduction"],
     }
+
+
+@pytest.mark.parametrize(
+    "slug, filename, frontmatter",
+    [
+        ("about-me", "test_data/about-me.md", FRONTMATTER1),
+        ("index", "vault/index.md", FRONTMATTER2),
+    ],
+)
+def test_note_initialization2(slug, filename, frontmatter):
+    content = f"""
+{frontmatter}
+
+This is a note about myself. You can find more in [[projects]] and [[hobbies]].
+    """
+    note = Note(
+        slug=slug,
+        filename=filename,
+        content=content,
+    )
+    assert note.content == content
+    assert note.frontmatter == {
+        "title": "About Me",
+        "tags": ["personal", "introduction"],
+    }
+    assert note.slug == slug
+    assert note.filename == filename.split("/")[-1]
 
 
 def test_extract_links(sample_note):
@@ -54,9 +85,7 @@ def test_extract_links(sample_note):
 
 def test_find_relevant_notes(vault):
     # Assuming `find_relevant_notes` method is implemented in ObsidianVault
-    relevant_notes = vault.find_relevant_notes(
-        start_slug="note2", max_hops=2, char_limit=100
-    )
+    relevant_notes = vault.find_relevant_notes(slug="note2", max_hops=2, char_limit=100)
     assert isinstance(relevant_notes, list)
     assert all("filename" in note for note in relevant_notes)
     assert all("contents_summary" in note for note in relevant_notes)
